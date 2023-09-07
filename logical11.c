ed8e04c4358b8625a7ae330e4643e3ef212940a8
InaamulHaque <143990733+InaamulHaque@users.noreply.github.com>
2023-09-07 22:43:25 +0530

    Add files via upload

---
+++ logical11.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main ()
+{
+	float d,a,b,c;
+	printf("Please enter the values of a,b,c\n");
+	scanf("%f%f%f", &a, &b, &c);
+	d = pow(b,2) - 4*a*c;
+	if (!(d==0))
+	{
+		printf("The roots are not equal");
+	}
+	else
+	{
+		printf("The roots are equal");
+	}
+	getche ();
+	return 0;
+}
